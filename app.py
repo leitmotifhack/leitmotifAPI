e85e8488abffa883134cfc753274c13188f8ee20
jeremyfisher314159 <jeremy.fisher@bd.com>
2018-10-27 23:27:44 -0700

    removed the complicated stuff

--- app.py
+++ app.py
@@ -1,53 +1,39 @@
-from flask import Flask, redirect, url_for, request
-from flask_restplus import Resource, Api
-from database import db_session
+from flask import Flask, request, render_template
 import pipeline
 import os
+from os import path
 from werkzeug.utils import secure_filename
-from uuid import uuid4
 
 
 # config
-UPLOAD_FOLDER = "./upload_folder"
-ALLOWED_EXTENSIONS = [".txt"]
 application = Flask(__name__)
-application.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-api = Api(
-    application,
-    version="0.1",
-    title="Leitmotif API",
-    description="API to serve the Leitmotif app",
-)
-
-quote_unquote_database = {}
+application.config["UPLOAD_FOLDER"] = "./upload_folder"
 
 
-@application.route("/uploadsnps", methods=["POST"])
+@application.route("/")
+def index():
+    return render_template('index.html')
+
+
+@application.route("/upload", methods=["GET", "POST"])
 def upload_file():
     """
     upload 23 and me data, convert to notes and redirect the user to a page
     that consumes those notes
     """
-    file = request.files["file"]
-    fp = os.path.join(
-        application.config["UPLOAD_FOLDER"], secure_filename(file.filename)
-    )
-    file.save(fp)
-    notes = pipeline.transform_23_and_me_dataset_to_notes(test_data)
-    notes_id = uuid4()
-    quote_unquote_database[notes_id] = notes
-    return redirect("frontend/music?notes_id={}".format(notes_id))
-
-
-@api.route("/notes/<notes_id>")
-class notes(Resource):
-    def get(self, notes_id):
-        return quote_unquote_database[notes_id]
-
-
-@application.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+    if request.method == 'POST':
+        file = request.files["file"]
+        fp = path.join(
+            application.config["UPLOAD_FOLDER"],
+            secure_filename(file.filename)
+        )
+        file.save(fp)
+        notes = pipeline.transform_23_and_me_dataset_to_notes(fp)
+        os.remove(fp)
+        return render_template("music.html",
+                               notes=notes)
+    elif request.method == 'GET':
+        return render_template('upload.html')
 
 
 if __name__ == "__main__":
