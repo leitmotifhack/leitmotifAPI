bf926be1d930924eeccfc6ecec148f548cb12abc
Raul Monraz <rmonrazecheverri@ebay.com>
2018-10-27 17:44:06 -0700

    database connection - Need to manually add secretes

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
+import secrets
 
-engine = create_engine('postgres://your_username:your_password@your_rds_subdomain.rds.amazonaws.com/your_database', convert_unicode=True)
+engine = create_engine('postgres://' + secrets.postgresqlusername + ':'+ secrets.postgresqlpassword + '@' + secrets.dbhost + '/' + secrets.dbname , convert_unicode=True)
 metadata = MetaData()
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
